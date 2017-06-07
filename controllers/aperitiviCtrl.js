3f06f0e15c66a9c6d9e8f30589f4c4066e7dd4da
stanLucian <lucian.stan@bestis.ro>
2017-06-07 16:05:30 +0300

    Update aperitiviCtrl.js

--- controllers/aperitiviCtrl.js
+++ controllers/aperitiviCtrl.js
@@ -1,13 +1,16 @@
 (function(){
 	"use strict";
-	angular.module("myApp").controller("aperitiviCtrl", ['$scope', '$routeParams', '$location', 'aperitivesServices', function($scope, $routeParams, $location, aperitivesServices){
-		$scope.aperitivi = "aperitive";
-
+	angular.module("myApp").controller("aperitiviCtrl", ['$scope', '$routeParams', 'aperitivesServices', 'productService', function($scope, $routeParams, aperitivesServices, productService){
+		
 		aperitivesServices.getAperitives(function(data){
 			$scope.allAperitives = data;
 		});
 
-		$scope.currentAperitive = $routeParams.id;
+		$scope.currentAperitive = productService.aperitive.filter(function(a){
+			return (a.id == $routeParams.id);
+		})[0];
+
+
 		/*angular.forEach($scope.allAperitives, function(aperitive){
 			if($routeParams.id == aperitive.id){
 				$scope.currentAperitive = aperitive;
@@ -17,6 +20,8 @@
 		aperitivesServices.getAperitiveDetails(function(id,data){
 			$scope.aperitiveDetails = data;
 		});*/
+
+
 		
 
 	}]);
