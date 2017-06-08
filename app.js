295172848342a3f1f8adbb9d3137a30429020645
stanLucian <lucian.stan@bestis.ro>
2017-06-08 14:57:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ var app = angular.module("myApp", ["ngRoute"]);
 
 app.config(function($routeProvider){
 		$routeProvider
-			.when("/", {templateUrl: "views/home.html", controller: "mainCtrl"})
+			//.when("/", {templateUrl: "index.html", controller: "aperitiviCtrl"})
 			.when("/aperitivi", {templateUrl: "views/aperitivi.html", controller: "aperitiviCtrl"})
 			.when("/menuDetails/:id", {templateUrl: "views/aperitivesDetails.html", controller: "aperitiviCtrl"})
-			.when("/aperitivi/:id", {templateUrl: "views/aperitivesDetails.html", controller: "aperitiviCtrl"})
 			.otherwise ({
 				redirectTo: "/"
 			});
