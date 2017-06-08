c3149da7d79f73e0b779e023ff31cce44d63ccc7
stanLucian <lucian.stan@bestis.ro>
2017-06-08 14:58:06 +0300

    Update aperitivesServices.js

--- services/aperitivesServices.js
+++ services/aperitivesServices.js
@@ -1,15 +1,16 @@
 (function(){
 	"use strict";
 
-	angular.module("myApp").service("aperitivesServices", ["sync", function(sync){
+	angular.module("myApp").service("aperitivesServices", ["sync", "$filter", function(sync, $filter){
 
 		this.getAperitives = function(cb){
 			sync.getAperitives(function(data){
 				cb(data);
 			});
 		};
-		this.getAperitiveDetails = function(cb){
-			sync.getAperitiveDetails(function(data){
+		
+		this.getAperitiveDetails = function(id, cb){
+			sync.getAperitiveDetails(id, function(data){
 				cb(data);
 			});
 		};
