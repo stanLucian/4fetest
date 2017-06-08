64b32ba0fbb0130e805c0e8d6565f6819dce487f
stanLucian <lucian.stan@bestis.ro>
2017-06-08 14:56:15 +0300

    Update aperitiviCtrl.js

--- controllers/aperitiviCtrl.js
+++ controllers/aperitiviCtrl.js
@@ -1,28 +1,47 @@
 (function(){
 	"use strict";
-	angular.module("myApp").controller("aperitiviCtrl", ['$scope', '$routeParams', 'aperitivesServices', 'productService', function($scope, $routeParams, aperitivesServices, productService){
-		
+	angular.module("myApp").controller("aperitiviCtrl", ['$scope', '$routeParams', 'aperitivesServices', 'productService','$http', function($scope, $routeParams, aperitivesServices, productService, $http){
+	
 		aperitivesServices.getAperitives(function(data){
-			$scope.allAperitives = data;
+			$scope.allAperitives = data.menus;
+			forEach
 		});
+		$scope.getAperitives = function(){
+			aperitivesServices.getAperitives(function(data){
+				$scope.allAperitives = data;
+			});
+		};
+		$scope.aperitivDetails = function(id) {
+			aperitivesServices.getAperitiveDetails(id, function(data){
+				$scope.getAperitives();
+			});
+		};
 
+		/*var baseUrl = "../api/";
+		$http({
+		  method: 'GET',
+		  url: baseUrl + 'database/menu-list.json',
+		}).then(function successCallback(response) {
+			$scope.data = response.data.menus;
+		    console.log($scope.data);
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
 		$scope.currentAperitive = productService.aperitive.filter(function(a){
 			return (a.id == $routeParams.id);
 		});
-		$scope.aperitiv = productService.aperitive[$routeParams.id];
+		//$scope.aperitiv = productService.aperitive[17]; 
 
 		/*angular.forEach($scope.allAperitives, function(aperitive){
 			if($routeParams.id == aperitive.id){
 				$scope.currentAperitive = aperitive;
 				breakAfter();
 			}
-		});
-		aperitivesServices.getAperitiveDetails(function(id,data){
-			$scope.aperitiveDetails = data;
 		});*/
-
-
-		
+		//aperitivesServices.getAperitiveDetails(function(id,data){
+		//	$scope.aperitiveDetails = data;
+	//	});
 
 	}]);
 }())
